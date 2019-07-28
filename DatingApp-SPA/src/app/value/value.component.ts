c40c51c2816d4b659fa45110e5f248476f1f8f7a
slawick12 <yaschenko.slawick12@gmail.com>
2019-07-28 17:42:56 +0200

    Display data from the API on the HTML

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -8,6 +8,7 @@ import { HttpClient } from "@angular/common/http";
 })
 export class ValueComponent implements OnInit {
   values: any;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -17,6 +18,7 @@ export class ValueComponent implements OnInit {
     this.http.get("http://localhost:5000/api/values").subscribe(
       response => {
         this.values = response;
+        console.log(response) 
       },
       error => {
         console.log(error);
