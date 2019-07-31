63a9e2e07c7fbeed47bfc3114bed0affd91a8e4c
slawick12 <yaschenko.slawick12@gmail.com>
2019-07-31 16:50:09 +0200

    using *ngIf to display html elements

--- DatingApp-SPA/src/app/navbar/navbar.component.ts
+++ DatingApp-SPA/src/app/navbar/navbar.component.ts
@@ -23,4 +23,12 @@ export class NavbarComponent implements OnInit {
       }
     );
   }
+  loggedIn() {
+    const token = localStorage.getItem("token");
+    return !!token;
+  }
+  logout() {
+    localStorage.removeItem("token");
+    console.log("logged out");
+  }
 }
