a42c63cc42e762657de1e29d0fcc2d1b0d789be4
slawick12 <yaschenko.slawick12@gmail.com>
2019-07-31 16:32:02 +0200

    Injecting services in components

--- DatingApp-SPA/src/app/navbar/navbar.component.ts
+++ DatingApp-SPA/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
 
 @Component({
   selector: "app-navbar",
@@ -8,11 +9,18 @@ import { Component, OnInit } from "@angular/core";
 export class NavbarComponent implements OnInit {
   model: any = {};
 
-  constructor() {}
+  constructor(private authSevice: AuthService) {}
 
   ngOnInit() {}
 
   login() {
-    console.log(this.model);
+    this.authSevice.login(this.model).subscribe(
+      next => {
+        console.log("Logged in successfully");
+      },
+      error => {
+        console.log("Failed to login");
+      }
+    );
   }
 }
