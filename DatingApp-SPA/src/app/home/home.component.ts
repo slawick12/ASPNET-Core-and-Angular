a42d7344884f332143cd94f442bbe872685a5c89
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-01 14:16:18 +0200

    output propeties input propeties

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -8,29 +8,16 @@ import { HttpClient } from "@angular/common/http";
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.getValues()
-  }
-
+  ngOnInit() {}
+  
   registerToggle() {
     this.registerMode = true;
   }
-  getValues() {
-    this.http.get("http://localhost:5000/api/values").subscribe(
-      response => {
-        this.values = response;
-        console.log(response);
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
-  cancelRegisterMode(registerMode: boolean){
-    this.registerMode=registerMode;
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
