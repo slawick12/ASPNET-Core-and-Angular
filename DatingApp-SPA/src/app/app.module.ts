c014dee413f45c0a3cb2545d04faae5bb687f721
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-02 16:04:09 +0200

    create guard for protecting routing

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { ListComponent } from './list/list.component';
 import { MessagesComponent } from './messages/messages.component'
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 
@@ -42,8 +43,8 @@ import { appRoutes } from './routes';
    providers: [
       ErrorInterceptorProvide,
       AuthService,
-      AlertifyService
-      
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
