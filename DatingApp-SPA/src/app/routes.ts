13a9a979eb47e51afaaf8f884296b76492c76b24
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-02 16:59:22 +0200

    using child to protection routing before login proces

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,12 +3,20 @@ import { HomeComponent } from "../app/home/home.component";
 import { MemberListComponent } from "./member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { ListComponent } from "./list/list.component";
-import { AuthGuard } from './_guards/auth.guard';
+import { AuthGuard } from "./_guards/auth.guard";
 
 export const appRoutes: Routes = [
-  { path: "home", component: HomeComponent },
-  { path: "members", component: MemberListComponent, canActivate:[AuthGuard] },
-  { path: "messages", component: MessagesComponent },
-  { path: "list", component: ListComponent },
-  { path: "**", redirectTo: "home", pathMatch: "full" }
+  {path: 'home', component: HomeComponent},
+  {
+path:"", 
+runGuardsAndResolvers:"always",
+canActivate:[AuthGuard],
+children: [
+  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'list', component: ListComponent},
+  
+]
+  },
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
 ];
