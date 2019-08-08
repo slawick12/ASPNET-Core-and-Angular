16e9a7ee314e72c565ca822df3ceed863a77045c
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-08 14:31:13 +0200

    adding interface to typescript code

---
+++ DatingApp-SPA/src/app/_moduls/photo.ts
@@ -0,0 +1,6 @@
+export interface Photo {
+  id: number;
+  description: string;
+  dateAdded: Date;
+  isMain: boolean;
+}
---
+++ DatingApp-SPA/src/app/_moduls/user.ts
@@ -0,0 +1,16 @@
+import { Photo } from './photo';
+
+export interface User {
+    id:number;
+    username:string;
+    knowAs:string;
+    age:number;
+    gender:string;
+    created: Date;
+    photoUrl:string;
+    country:string;
+    interest?: string;
+    introduction?:string;
+    lokingFor?:string;
+    photos?:Photo[];
+}
