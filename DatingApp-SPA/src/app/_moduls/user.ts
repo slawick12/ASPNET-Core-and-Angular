79a9d51602d10d42ef711ca761deeb9224d90336
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-08 14:32:06 +0200

    interface to typescript code

--- DatingApp-SPA/src/app/_moduls/user.ts
+++ DatingApp-SPA/src/app/_moduls/user.ts
@@ -1,16 +1,17 @@
-import { Photo } from './photo';
+import { Photo } from "./photo";
 
 export interface User {
-    id:number;
-    username:string;
-    knowAs:string;
-    age:number;
-    gender:string;
-    created: Date;
-    photoUrl:string;
-    country:string;
-    interest?: string;
-    introduction?:string;
-    lokingFor?:string;
-    photos?:Photo[];
+  id: number;
+  username: string;
+  knowAs: string;
+  age: number;
+  gender: string;
+  created: Date;
+  photoUrl: string;
+  country: string;
+  interest?: string;
+  introduction?: string;
+  lokingFor?: string;
+  photos?: Photo[];
 }
+
