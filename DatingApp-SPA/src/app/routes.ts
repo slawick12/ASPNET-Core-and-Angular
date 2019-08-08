8fdd3875aa9b421d0bcd65c0789a836d8bae25b4
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-08 19:52:08 +0200

    Create member component and     view

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -4,19 +4,20 @@ import { MemberListComponent } from "./members/member-list/member-list.component
 import { MessagesComponent } from "./messages/messages.component";
 import { ListComponent } from "./list/list.component";
 import { AuthGuard } from "./_guards/auth.guard";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
 
 export const appRoutes: Routes = [
-  {path: 'home', component: HomeComponent},
+  { path: "home", component: HomeComponent },
   {
-path:"", 
-runGuardsAndResolvers:"always",
-canActivate:[AuthGuard],
-children: [
-  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-  {path: 'messages', component: MessagesComponent},
-  {path: 'list', component: ListComponent},
-  
-]
+    path: "",
+    runGuardsAndResolvers: "always",
+    canActivate: [AuthGuard],
+    children: [
+      { path: "members", component: MemberListComponent },
+      { path: "members/:id", component: MemberDetailComponent },
+      { path: "messages", component: MessagesComponent },
+      { path: "list", component: ListComponent }
+    ]
   },
-  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  { path: "**", redirectTo: "home", pathMatch: "full" }
 ];
