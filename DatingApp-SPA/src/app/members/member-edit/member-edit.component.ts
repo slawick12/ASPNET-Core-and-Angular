5aae9df9bc3d5ffb354873d6e0ce3a25d2130c34
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-11 15:37:53 +0200

    createng the member edit template and adding a CanActive route guard

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, HostListener } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { User } from "src/app/_moduls/user";
+import { AlertifyService } from "src/app/_services/alertify.service";
+import { NgForm } from "@angular/forms";
+import { Hosted } from 'protractor/built/driverProviders';
 
 @Component({
   selector: "app-member-edit",
@@ -8,12 +11,29 @@ import { User } from "src/app/_moduls/user";
   styleUrls: ["./member-edit.component.css"]
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm", {static: true}) editForm: NgForm;
+  @HostListener('window:beforeunload', ["$event"])
+  unloadNotification($event: any){
+    if(this.editForm.dirty){
+      $event.returnValue =true;
+    }
+  }
+
   user: User;
-  constructor(private route: ActivatedRoute) {}
+
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data["user"];
     });
+  }
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success("Updated successfully");
+    this.editForm.reset(this.user);
   }
 }
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -9,6 +9,7 @@ import { MemberDatailResolver } from "./_resolvers/member-detail.resolver";
 import { MemberListResolver } from "./_resolvers/member-list.resolver";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 
 export const appRoutes: Routes = [
   { path: "home", component: HomeComponent },
@@ -17,9 +18,22 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: "always",
     canActivate: [AuthGuard],
     children: [
-      {path: "members",component: MemberListComponent,resolve: { users: MemberListResolver }},
-      {path: "members/:id",component: MemberDetailComponent,resolve: { user: MemberDatailResolver }},
-      {path: "member/edit",component: MemberEditComponent,resolve: { user: MemberEditResolver }},
+      {
+        path: "members",
+        component: MemberListComponent,
+        resolve: { users: MemberListResolver }
+      },
+      {
+        path: "members/:id",
+        component: MemberDetailComponent,
+        resolve: { user: MemberDatailResolver }
+      },
+      {
+        path: "member/edit",
+        component: MemberEditComponent,
+        resolve: { user: MemberEditResolver },
+        canDeactivate: [PreventUnsavedChanges]
+      },
       { path: "messages", component: MessagesComponent },
       { path: "list", component: ListComponent }
     ]
