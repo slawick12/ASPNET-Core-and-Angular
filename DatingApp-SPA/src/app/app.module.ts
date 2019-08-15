a01c345ad8cedf56a9a1c3629bff64c7bd9a419c
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-15 13:40:13 +0200

    Validation in Reactive form

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { JwtModule } from "@auth0/angular-jwt";
 import { NgxGalleryModule } from "ngx-gallery";
@@ -50,6 +50,7 @@ export function tokenGetter() {
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgxGalleryModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output } from "@angular/core";
 import { EventEmitter } from "@angular/core";
 import { AuthService } from "../_services/auth.service";
 import { AlertifyService } from "../_services/alertify.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -12,22 +13,31 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
+
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm= new FormGroup({
+      username: new FormControl("Hello", Validators.required),
+      password: new FormControl(" ", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    })
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertify.success("Register successfull");
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success("Register successfull");
+    //   },
+    //   error => {
+    //     this.alertify.error(error);
+    //   }
+    // );
+    console.log(this.registerForm.value);
   }
   cancel() {
     this.cancelRegister.emit(false);
