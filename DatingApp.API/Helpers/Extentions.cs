e5c9620c09077001442ab62c9d6f6991b791091f
slawick12 <yaschenko.slawick12@gmail.com>
2019-08-17 12:23:36 +0200

    settings up the paging helper class

--- DatingApp.API/Helpers/Extentions.cs
+++ DatingApp.API/Helpers/Extentions.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace DatingApp.API.Helpers
 {
@@ -11,11 +12,18 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Access-Control-Expose-Headers", "Aplication-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
+
+        public static void AddPagination(this HttpResponse response, int currentPage, int totalItems, int totalPages, int itemsPerPage)
+        {
+                var paginationHeader = new PaginationHeader(currentPage,itemsPerPage,totalItems, totalPages);
+                response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+                response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
         public static int CalculateAge(this DateTime theDateTime)
         {
             var age = DateTime.Today.Year - theDateTime.Year;
-            if(theDateTime.AddYears(age) > DateTime.Today)
-            age--;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
             return age;
         }
     }
